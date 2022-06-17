10090c079d8a5434980fc147f6375c78bb797270
David Hudson <davidhudsont@gmail.com>
2022-06-16 19:14:30 -0700

    Removed dead code.

--- src/main.rs
+++ src/main.rs
@@ -14,41 +14,15 @@ fn main() {
         .stop_bits(StopBits::One);
 
     let mut port = builder.open().expect("Failed to open port");
-
     port.set_timeout(Duration::new(1, 0)).unwrap();
-    // port.reconfigure(&|settings: &mut dyn SerialPortSettings| {
-    //     settings.set_baud_rate(serial::Baud115200)?;
-    //     settings.set_char_size(serial::Bits8);
-    //     settings.set_parity(serial::ParityNone);
-    //     settings.set_stop_bits(serial::Stop1);
-    //     settings.set_flow_control(serial::FlowNone);
-    //     Ok(())
-    // }).unwrap();
+
     let mut xmodem: XModem = XModem::new(port);
 
-    let stream = File::open("example.txt").unwrap();
+    // let stream = File::open("example.txt").unwrap();
+    // xmodem.send(Box::new(stream)).unwrap();
 
-    xmodem.send(Box::new(stream)).unwrap();
+    let stream = File::create("example2.txt").unwrap();
+    xmodem.recieve(Box::new(stream), false).unwrap();
 
-    // interact(&mut port).unwrap();
 }
 
-// fn interact<T: SerialPort>(port: &mut T) -> io::Result<()> {
-//     port.reconfigure(&|settings: &mut dyn SerialPortSettings| {
-//         settings.set_baud_rate(serial::Baud9600)?;
-//         settings.set_char_size(serial::Bits8);
-//         settings.set_parity(serial::ParityNone);
-//         settings.set_stop_bits(serial::Stop1);
-//         settings.set_flow_control(serial::FlowNone);
-//         Ok(())
-//     })?;
-
-//     port.set_timeout(Duration::from_millis(1000))?;
-
-//     let mut buf: Vec<u8> = (0..255).collect();
-
-//     port.write(&buf[..])?;
-//     port.read(&mut buf[..])?;
-
-//     Ok(())
-// }
